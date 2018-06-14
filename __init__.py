0e0fdbc20018ec6ce38919e013b5b07e646fe51f
ranjian0 <karanjaichungwa@gmail.com>
2018-06-14 18:46:42 +0300

    add __init__.py

---
+++ __init__.py
@@ -0,0 +1,18 @@
+bl_info = {
+    "name": "Landscape Tools",
+    "author": "Ian Ichung'wa Karanja (ranjian0)",
+    "version": (0, 0, 1),
+    "blender": (2, 79, 0),
+    "location": "View3D > Toolshelf > Landscape Tools",
+    "description": "Landscape Creation Tools",
+    "warning": "",
+    "wiki_url": "",
+    "tracker_url": "",
+    "category": "Cynthia"
+}
+
+def register():
+    pass
+
+def unregister():
+    pass
